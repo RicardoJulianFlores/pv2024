67bae0666971b70efd1ef754939beb336518bf01
Ricardo Julián Flores <131063701+RicardoJulianFlores@users.noreply.github.com>
2024-04-21 09:15:25 -0300

    Ejercicio 15 - Array

---
+++ tp1_FloresRicardo/src/ar/edu/unju/fi/ejercicio15/Main.java
@@ -0,0 +1,34 @@
+package ar.edu.unju.fi.ejercicio15;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String[] nombre;
+		int max;
+	
+		do {
+		System.out.println("Ingrese numero entre 5 y 10: ");
+		max = sc.nextInt();
+		}while(max<5 || max>10);
+		nombre = new String[max];
+		sc.nextLine();
+		for(int i=0;i<max;i++)
+		{
+			System.out.println("Ingrese nombre en la posicion "+i+": ");
+			nombre[i] = sc.next();
+		}
+		for(int i=0;i<max;i++)
+		{	
+			System.out.println("Nombre "+i+" : " +nombre[i]);
+		}
+		System.out.println();
+		for(int i=max-1;i>=0;i--)
+		{	
+			System.out.println("Nombre "+i+" : " +nombre[i]);
+		}
+	}
+
+}
